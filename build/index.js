5f2e2af35e60e2327f9857862427bd102b13a133
mkhusid <max6hc@gmail.com>
2020-06-14 06:29:01 +0300

    [common] remove build & modules

--- build/index.js
+++
@@ -1,7 +0,0 @@
-const logParser = require('./LogParser');
-const main = async () => {
-    let hostsAppeareceCounter = new logParser('cs-host', 'fields', process.argv[2]);
-    await hostsAppeareceCounter.parseLogs();
-    hostsAppeareceCounter.countAppearence();
-};
-main().catch(e => console.log(`[${e.name}]: ${e.message}`));
